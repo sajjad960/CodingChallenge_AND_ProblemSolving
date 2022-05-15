73b99dead124b019769893ea6dacf05745c94204
sajjad960 <sajjadhossainsb01@gmail.com>
2022-05-15 21:17:27 +0600

    learned solve and simplify our code

---
+++ test.js
@@ -0,0 +1,16 @@
+"use-strict"
+function checkK(array, k) {
+    let = array[0];
+
+    for (let j = 0; j < array.length; j++) {
+        if (i < array[i] && j === i) {
+
+        }
+
+    }
+
+}
+
+const array = [1, 2, 3, 4, 5, 5]
+const k = 3
+checkK(array, 3)
