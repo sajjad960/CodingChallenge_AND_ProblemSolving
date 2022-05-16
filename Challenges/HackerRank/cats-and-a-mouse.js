5eaa873b30df0abb5c894305985cd088fc8ff592
sajjad960 <sajjadhossainsb01@gmail.com>
2022-05-16 23:31:18 +0600

    added a problem solving

--- Challenges/HackerRank/cats-and-a-mouse.js
+++ Challenges/HackerRank/cats-and-a-mouse.js
@@ -1,8 +1,4 @@
-// declare a variable to store return cat
-// check this logic
-// z -x and z - y and return which one give less unit
-// if unit is same for both cat return Mouse C
-
+// Complete the catAndMouse function below.
 function catAndMouse(x, y, z) {
     let result = '';
     const CatAUnit = Math.abs(z - x);
@@ -12,5 +8,3 @@ function catAndMouse(x, y, z) {
     CatAUnit < CatBUnit ? result = 'Cat A' : result = "Cat B";
     return result;
 }
-
-console.log(catAndMouse(21, 95, 58));
