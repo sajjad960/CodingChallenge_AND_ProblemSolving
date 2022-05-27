f347db07478d40c8cc5146f117633e81f58fcc54
sajjad960 <sajjadhossainsb01@gmail.com>
2022-05-27 21:28:22 +0600

    countUniqueValues

---
+++ JS_Algo_DataStructure/ProblemSolvingPattern/multiplePointers/challange.js
@@ -0,0 +1,17 @@
+function countUniqueValue(array) {
+    // let left = 0;
+    // let right = array.length - 1;
+    // while () {
+
+    // }
+    let i = 0;
+    for (let j = 0; j < array.length; j++) {
+        if (array[i] !== array[j]) {
+            i++;
+            array[i] = array[j]
+        }
+    }
+    return i;
+}
+
+console.log(countUniqueValue([1, 1, 1, 1, 1, , 2]));
