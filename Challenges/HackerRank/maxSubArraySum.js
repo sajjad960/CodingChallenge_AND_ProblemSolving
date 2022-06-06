24fd682fe72c079714d6a8756d6d4cfe60b25220
sajjad960 <sajjadhossainsb01@gmail.com>
2022-06-06 21:00:14 +0500

    working on slidewindow

---
+++ Challenges/HackerRank/maxSubArraySum.js
@@ -0,0 +1,25 @@
+function maxSubArraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+
+    if (num > arr.length) return null;
+
+    // first loop for calculating 
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i]
+    }
+    // change the first positon of sum and also last position
+    tempSum = maxSum
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum)
+    }
+    console.log(maxSum);
+    return maxSum;
+}
+
+maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2)
+maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 4)
+maxSubArraySum([4, 2, 1, 6], 1)
+maxSubArraySum([4, 2, 1, 6], 4)
+maxSubArraySum([], 4)
