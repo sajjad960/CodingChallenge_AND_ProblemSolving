bb613ae2900f88d5878c030c974e95e08ef597f9
sajjad960 <sajjadhossainsb01@gmail.com>
2022-06-17 17:59:22 +0500

    writted recursion

---
+++ JS_Algo_DataStructure/Recursion/Write_Factorial_Iteravtively/factorial.js
@@ -0,0 +1,16 @@
+// without recursion ,factorial
+function factorial(num) {
+    let total = 1;
+    for (let i = num; i > 1; i--) {
+        total *= i
+    }
+    return total
+}
+console.log(factorial(4));
+
+// with recursion ,factorial
+function recursiveFactorial(num) {
+    if (num === 1) return 1;
+    return num * recursiveFactorial(num - 1)
+}
+console.log(recursiveFactorial(4));
